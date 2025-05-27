47c77d6daf3796e6a158cd0b5eb659ab8625b417
ammita-ganddhi <160031455+ammita-ganddhi@users.noreply.github.com>
2025-05-28 01:57:23 +0530

    Devnagari Script

--- src/main/java/DevnagariScript.java
+++ src/main/java/DevnagariScript.java
@@ -3,3 +3,11 @@ public class DevnagariScript {
         System.out.println("\u0905\u092E\u093F\u0924\u093E");
     }
 }
+
+
+/*
+"C:\Program Files\Java\jdk-21\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.1\lib\idea_rt.jar=50801:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.1\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath D:\AutomationLearning\target\classes DevnagariScript
+अमिता
+
+Process finished with exit code 0
+*/
